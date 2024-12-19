7442f8b7c4d7bc325ccbe59f81a867ce30019eaf
umarkhan135 <khanm417@mcmaster.ca>
2024-12-19 13:27:21 -0500

    sponsors page complete for now

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,13 +1,15 @@
-import ParticlesHexagon from "@/components/ParticlesHexagon";
 import Sponsors from "@/components/Sponsors";
 import React from "react";
+import ParticlesHexagon from "./ParticlesHexagon";
+
 
 function SponsorPage() {
   return (
-    <div className="w-full h-full">
-      {/* <ParticlesHexagon id="test-parts"></ParticlesHexagon> */}
-      <Sponsors />
-    </div>
+    // <div className="w-full h-full">
+    //   <Sponsors />
+    // </div>
+    
+    <Sponsors />
   );
 }
 
