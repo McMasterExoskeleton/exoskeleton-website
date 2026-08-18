1fadf87e7b1ca0b74cba109c3d71e520c4eebc22
majockbim <majock.bim1@gmail.com>
2026-08-18 19:44:20 -0400

    add electrical app link

--- app/recruiting/constants.ts
+++ app/recruiting/constants.ts
@@ -3,5 +3,5 @@ export const GENERAL_APPLICATION_LINK = ""; // Used on the main recruiting page
 
 export const SOFTWARE_APPLICATION_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfGezcc82_VxnPqHvZ0kKeI2O6d2IdpBwioZ384lmIOoBBsNg/viewform?usp=header";
 export const MECHANICAL_APPLICATION_LINK = "";
-export const ELECTRICAL_APPLICATION_LINK = "";
+export const ELECTRICAL_APPLICATION_LINK = "https://docs.google.com/forms/d/1TgECvCQzjAMOKfZkr99vq2HTz1ZFvZF6T5yEeRuVL1Y/edit?usp=drivesdk";
 export const BUSINESS_APPLICATION_LINK = "";
