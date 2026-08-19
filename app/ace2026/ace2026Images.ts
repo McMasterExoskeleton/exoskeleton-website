9541b6b14e87f753eb5b7a607fecfe7f8fd903b4
majockbim <majock.bim1@gmail.com>
2026-08-18 23:01:38 -0400

    add pictures to ace_2026

---
+++ app/ace2026/ace2026Images.ts
@@ -0,0 +1,13 @@
+// List of images for the ACE 2026 gallery.
+// To update the gallery, add or remove image filenames from the public/ace_2026 directory and update this array accordingly.
+
+const ace2026Images = [
+  "/ace_2026/DSCF0584.jpeg",
+  "/ace_2026/IMG_4128.JPG",
+  "/ace_2026/IMG_4369.jpg",
+  "/ace_2026/IMG_4563.jpg",
+  "/ace_2026/IMG_4800.JPG",
+  "/ace_2026/sponsors.jpg",
+];
+
+export default ace2026Images;
